19f43c2daad7538f130896eee8ac87fb754ea9ab
ONOUEKENTO <kentou1506@gmail.com>
2021-10-30 15:05:27 +0900

    最終のシーケンス作成中

---
+++ Assets/_MyFolder/Scripts/DebugOnEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// デバッグ用キー
+/// </summary>
+public class DebugOnEditor : MonoBehaviour
+{
+    void Update()
+    {
+        if(!Application.isEditor) return;
+        
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            Time.timeScale *= 2f;
+            Debug.Log($"TimeScale:{Time.timeScale}");
+        }
+        else if (Input.GetKeyDown(KeyCode.F2))
+        {
+            Time.timeScale = 1f;
+            Debug.Log($"TimeScale:{Time.timeScale}");
+        }
+    }
+}
